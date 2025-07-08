a7cf5b39bffa5a9594d522a6a61f72e3d6246a45
luckyhut <ed7johnson@gmail.com>
2025-07-07 21:01:09 -0600

    Accepts system prompt as argument

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
+    if len(sys.argv) < 2:
+        print("Error: prompt is required as an argument")
+        sys.exit(1)
+        
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
 
-
     response = client.models.generate_content(model="gemini-2.0-flash-001",
-                                              contents="Why is boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-                                              
-
+                                              contents=sys.argv[1])
+    
     print(response.text)
-    # print(f"User prompt: { user_prompt }")
     print(f"Prompt tokens: { response.usage_metadata.prompt_token_count }")
     print(f"Response tokens: { response.usage_metadata.candidates_token_count }")
 
