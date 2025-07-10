95f2b7d6ade0d8c345787557e461656e0778d93e
luckyhut <ed7johnson@gmail.com>
2025-07-09 18:49:41 -0600

    Added llm function to read from a file

---
+++ functions/get_file_content.py
@@ -0,0 +1,26 @@
+import os
+
+def get_file_content(working_directory, file_path):
+    wabs_path = os.path.abspath(working_directory)
+    target_path = os.path.join(working_directory, file_path)
+    joined_path = os.path.abspath(target_path)
+
+    # outside check
+    if not joined_path.startswith(wabs_path):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    # exists check
+    if not os.path.isfile(joined_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    try:
+        file_content_string = ''
+        MAX_CHARS = 10000
+        with open(joined_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+        if len(file_content_string) == 10000:
+            file_content_string += '[...File "{file_path}" truncated at 10000 characters].'
+    except Exception as e:
+        return f'Error: {e}'
+
+    return file_content_string
--- tests.py
+++ tests.py
@@ -1,11 +1,19 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 
 def test():
-    print(get_files_info("calculator", "."))
-    print(get_files_info("calculator", "pkg"))
-    print(get_files_info("calculator", "/bin"))
-    print(get_files_info("calculator", "../main.py"))
+    
+    ## get file content tests
+    print(get_file_content("calculator", "main.py"))
+    print(get_file_content("calculator", "pkg/calculator.py"))
+    print(get_file_content("calculator", "/bin/cat"))
+
+    ## get files info tests
+    # print(get_files_info("calculator", "."))
+    # print(get_files_info("calculator", "pkg"))
+    # print(get_files_info("calculator", "/bin"))
+    # print(get_files_info("calculator", "../main.py"))
 
 if __name__ == "__main__":
     test()
