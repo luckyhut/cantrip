2b182cce318c2612927c7415a1f74b7a8cb76f88
luckyhut <ed7johnson@gmail.com>
2025-07-13 15:51:10 -0600

    LLM can use get_file_content (no output yet)

---
+++ functions/run_python.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path):
+    wabs_path = os.path.abspath(working_directory)
+    target_path = os.path.join(working_directory, file_path)
+    joined_path = os.path.abspath(target_path)
+
+    # outside check
+    if not joined_path.startswith(wabs_path):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+    # exists check
+    if not os.path.exists(joined_path):
+        return f'Error: File "{file_path}" not found'
+
+    if not file_path.endswith('.py'):
+        return f'Error: "{file_path}" is not a Python file'
+
+    try:
+        run_result = subprocess.run(["python", joined_path], capture_output=True, timeout=30, cwd=wabs_path)
+
+        result = []
+        if run_result.stdout or run_result.stderr:
+            result.append("STDOUT:" + run_result.stdout.decode() + "\n")
+            result.append("STDERR:" + run_result.stderr.decode())
+
+        if run_result.returncode != 0:
+            result.append(f'\nProcess exited with code {run_result.returncode}\n')
+
+        return "\n".join(result) if result else "No output produced."
+    
+    except Exception as e:
+        return f'Error: executing Python file: {e}'
